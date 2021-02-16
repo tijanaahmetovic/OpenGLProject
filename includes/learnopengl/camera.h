da6e34770ed643be251a310d476e48f71bf0b235
tijana <tijana_ahmetovic@hotmail.com>
2021-02-16 01:08:19 +0100

    final

--- includes/learnopengl/camera.h
+++ includes/learnopengl/camera.h
@@ -17,8 +17,8 @@ enum Camera_Movement {
 
 // Default camera values
 const float YAW         = -90.0f;
-const float PITCH       =  0.0f;
-const float SPEED       =  2.5f;
+const float PITCH       =  -25.0f;
+const float SPEED       =  20.0f;
 const float SENSITIVITY =  0.1f;
 const float ZOOM        =  45.0f;
 
--- src/solar_system/solar_system/solar_system.cpp
+++ src/solar_system/solar_system/solar_system.cpp
@@ -21,10 +21,10 @@ void processInput(GLFWwindow *window);
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
-const int NUM = 9;
+const int NUM = 22;
 
 // camera
-Camera camera(glm::vec3(0.0f, 0.0f, 500.0f));
+Camera camera(glm::vec3(0.0f, 50.0f, 100.0f));
 float lastX = (float)SCR_WIDTH / 2.0;
 float lastY = (float)SCR_HEIGHT / 2.0;
 bool firstMouse = true;
@@ -82,31 +82,62 @@ int main()
     // load models
     // -----------
 
-  
-    Model** solarSystem = new Model*[NUM];
-    solarSystem[0] = new Model(FileSystem::getPath("resources/objects/planets/sun/sun.obj"));
-    solarSystem[1] = new Model(FileSystem::getPath("resources/objects/planets/mercury/mercury.obj"));
-    solarSystem[2] = new Model(FileSystem::getPath("resources/objects/planets/venus/venus.obj"));
-    solarSystem[3] = new Model(FileSystem::getPath("resources/objects/planets/earth/earth.obj"));
-    solarSystem[4] = new Model(FileSystem::getPath("resources/objects/planets/mars/mars.obj"));
-    solarSystem[5] = new Model(FileSystem::getPath("resources/objects/planets/jupiter/jupiter.obj"));
-    solarSystem[6] = new Model(FileSystem::getPath("resources/objects/planets/saturn/saturn.obj"));
-    solarSystem[7] = new Model(FileSystem::getPath("resources/objects/planets/uranus/uranus.obj"));
-    solarSystem[8] = new Model(FileSystem::getPath("resources/objects/planets/neptune/neptune.obj"));
-   
-    float distanceFromSun[NUM] = { 0.0f, 80.0f, 100.0f, 150.0f, 200.0f, 250.0f, 300.0f, 350.0f, 400.0f };
-    float rotationSpeed[NUM] = { 2.0f, 29.43f, -21.76f, 18.5f, 14.91f, 8.51f, 6.0f, 4.225f, 3.374f };
-    float revolutionSpeed[NUM] = { 0.0f, 47.0f, 35.0f, 29.0f, 24.0f, 13.0f, 9.69f, 6.81f, 5.43f };
-    float scaleFactor[NUM] = {5.0f, 2.0f, 2.0f, 2.0f, 2.0f, 1.0f, 1.0f, 1.5f, 1.5f};
-    glm::vec3 revolutionAxis =  glm::vec3(0.0f, 1.0f, 0.0f);
     
+    Model** solarSystem = new Model*[NUM];
+   
+    std::string path = "resources/objects/planets/";
+    std::string objects[NUM] = {"sun/sun.obj", "mercury/mercury.obj", "venus/venus.obj", "earth/earth.obj", "earth/earth_moon.obj", 
+        "mars/mars.obj", "mars/mars_moon_one.obj","mars/mars_moon_two.obj",
+        "jupiter/jupiter.obj", "jupiter/jupiter_moon_1.obj", "jupiter/jupiter_moon_2.obj","jupiter/jupiter_moon_3.obj","jupiter/jupiter_moon_4.obj",
+        "saturn/saturn.obj", "saturn/saturn_moon_1.obj","saturn/saturn_moon_2.obj","saturn/saturn_moon_3.obj",
+        "uranus/uranus.obj", "uranus/uranus_moon_1.obj", "uranus/uranus_moon_2.obj", "uranus/uranus_moon_3.obj",
+        "neptune/neptune.obj"};
+    for (int i = 0; i < NUM; i++) {
+        solarSystem[i] = new Model(FileSystem::getPath(path+objects[i]));
+    }
+
+   
+    float distanceFromSun[NUM] = 
+      { 0.0f, 12.0f, 15.0f, 20.0f, 20.0f, 
+        25.0f, 25.0f, 25.0f, 
+        35.0f, 35.0f,35.0f,35.0f,35.0f, 
+        45.0f, 45.0f, 45.0f, 45.0f, 
+        55.0f, 55.0f, 55.0f, 55.0f, 
+        65.0f};
+
+    float rotationSpeed[NUM] = 
+      { 2.0f, 29.43f, -21.76f, 5.5f, 18.5f, 
+        5.91f, 14.91f, 10.91f, 
+        8.51f, 7.51f, 12.51f, 10.51f, 5.51f, 
+        5.0f, 10.0f, 15.0f, 8.0f, 
+        4.225f, 8.225f, 14.225f, 4.225f, 
+        3.374f };
+
+    float revolutionSpeed[NUM] = 
+    { 0.0f, 47.0f, 35.0f, 29.0f, 29.0f, 
+        24.0f, 24.0f, 24.0f, 
+        13.0f, 13.0f, 13.0f, 13.0f, 13.0f, 
+        9.69f, 9.69f, 9.69f, 9.69f, 
+        6.81f, 6.81f, 6.81f, 6.81f,
+        5.43f};
+
+    float scaleFactor[NUM] = 
+    { 1.0f, 0.5f, 0.5f, 0.5f, 0.5f, 
+        0.5f, 0.5f, 0.5f, 
+        0.1f, 0.1f, 0.1f, 0.1f, 0.1f,
+        0.1f, 0.1f, 0.1f, 0.1f, 
+        0.1f, 0.1f, 0.1f, 0.1f,
+        0.1f};
+
+    glm::vec3 revolutionAxis =  glm::vec3(0.0f, 1.0f, 0.0f);
 
     glm::vec3* rotationAxis = new glm::vec3[NUM];
     for (int i = 0; i < NUM; i++) {
         rotationAxis[i] = glm::vec3(0.0f, 1.0f, 0.0f);
     }
-    rotationAxis[7] = glm::vec3(1.0f, 0.0f, 0.0f);
-
+    rotationAxis[17] = glm::vec3(1.0f, 0.0f, 0.0f);//uranus
+   
+   
 
     // render loop
     // -----------
@@ -128,7 +159,7 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // configure transformation matrices
-        glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 2000.0f);
+        glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 200.0f);
         glm::mat4 view = camera.GetViewMatrix();
         shader.use();
         shader.setMat4("projection", projection);
@@ -145,10 +176,8 @@ int main()
             model = glm::scale(model, glm::vec3(scaleFactor[i], scaleFactor[i], scaleFactor[i]));
             shader.setMat4("model", model);
             solarSystem[i]->Draw(shader);
-        
         }
-
- 
+     
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
@@ -167,13 +196,13 @@ void processInput(GLFWwindow *window)
         glfwSetWindowShouldClose(window, true);
 
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-        camera.ProcessKeyboard(FORWARD, deltaTime * 20.0f);
+        camera.ProcessKeyboard(FORWARD, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-        camera.ProcessKeyboard(BACKWARD, deltaTime * 20.0f);
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-        camera.ProcessKeyboard(LEFT, deltaTime * 20.0f);
+        camera.ProcessKeyboard(LEFT, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-        camera.ProcessKeyboard(RIGHT, deltaTime * 20.0f);
+        camera.ProcessKeyboard(RIGHT, deltaTime);
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
